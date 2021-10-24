a28311fe50b106569304adfc8e0e99937e9a1b28
Andrei Kovaci <andrei.kovaci@gmail.com>
2021-10-24 19:07:26 +0100

    find the numerical passwords

--- index.php
+++ index.php
@@ -8,6 +8,15 @@ $db_username = 'root';
 $db_password = '';
 $db_name = 'passwords_test';
 
+$passwordsList = [];
+
+###
+define('SALT','ThisIs-A-Salt123');
+function salter($string){
+    return md5($string . SALT);
+}
+###
+
 try
 {
     $conn = new PDO("mysql:host=$db_server;dbname=$db_name", $db_username, $db_password);
@@ -18,7 +27,8 @@ try
     $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
 
     foreach ($stmt->fetchAll() as $k => $v) {
-        echo "<tr><td>" . $v["user_id"] . "</td><td>" . $v["password"] . "</td></tr>";
+//        echo "<tr><td>" . $v["user_id"] . "</td><td>" . $v["password"] . "</td></tr>";
+        $passwordsList[$v["user_id"]] = $v["password"];
     }
 }
 catch (PDOException $e)
@@ -28,4 +38,33 @@ catch (PDOException $e)
 
 $conn = null;
 echo "</table>";
+
+echo "List of passwords: " . count($passwordsList) . " items<br>";
+
+
+function checkHashAgainstPasswordHash($hash, $passwordsList) {
+    foreach ($passwordsList as $key => $value) {
+        if ($hash == $value) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+echo "<br><br><br>";
+echo "Passwords made out of 5 numbers<br>";
+// get the 5 number passwords
+for ($i = 10000; $i < 100000; $i++) {
+    $passToGuess = salter($i);
+
+    if (checkHashAgainstPasswordHash($passToGuess, $passwordsList)) {
+        echo "Found " . $i . "  " . $passToGuess . "<br>";
+    }
+
+}
+
+echo "<br><br><br>";
+echo "Passwords made out of 3 capital letters and 1 number<br>";
+
 
