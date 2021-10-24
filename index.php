6de00c775eb772ef94a5b903fd5939ef8fb20550
Andrei Kovaci <andrei.kovaci@gmail.com>
2021-10-24 22:07:43 +0100

    added table for the 5 digit passwords

--- index.php
+++ index.php
@@ -9,15 +9,26 @@ $db_name = 'passwords_test';
 
 $passwordsList = [];
 
+// some useful constants
+const FORM_SUBMIT_VALUE = "crack";
+
+const LOWERCASE_LETTERS = 'a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z';
+const UPPERCASE_LETTERS = 'A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z';
+const DIGITS = '0,1,2,3,4,5,6,7,8,9';
+
 try
 {
+    // use PDO to connect to the databse
     $conn = new PDO("mysql:host=$db_server;dbname=$db_name", $db_username, $db_password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // prepare a statement to load the data from the passwords table
     $stmt = $conn->prepare("Select user_id, password FROM not_so_smart_users");
     $stmt->execute();
 
     $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
 
+    // save all the passwords in memory
     foreach ($stmt->fetchAll() as $k => $v) {
         $passwordsList[$v["user_id"]] = $v["password"];
     }
@@ -27,7 +38,116 @@ catch (PDOException $e)
     echo "Error: " . $e->getMessage();
 }
 
+// close the connection
 $conn = null;
+
+
+### given hash function
+define('SALT','ThisIs-A-Salt123');
+function salter($string){
+    return md5($string . SALT);
+}
+###
+
+/**
+ * @param $hash - the generated hash
+ * @param $passwordsList - the list of passwords
+ * @return bool - true if the hash matches
+ */
+function checkHashAgainstPasswordHash($hash, $passwordsList) {
+    foreach ($passwordsList as $key => $value) {
+        if ($hash === $value) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+/**
+ * This function has been taken from
+ * https://stackoverflow.com/questions/19067556/php-algorithm-to-generate-all-combinations-of-a-specific-size-from-a-single-set
+ *
+ * @param $chars
+ * @param $size
+ * @param array $combinations
+ * @return array|mixed
+ */
+function sampling($chars, $size, $combinations = array()) {
+
+    # if it's the first iteration, the first set
+    # of combinations is the same as the set of characters
+    if (empty($combinations)) {
+        $combinations = $chars;
+    }
+
+    # we're done if we're at size 1
+    if ($size == 1) {
+        return $combinations;
+    }
+
+    # initialise array to put new values in
+    $new_combinations = array();
+
+    # loop through existing combinations and character set to create strings
+    foreach ($combinations as $combination) {
+        foreach ($chars as $char) {
+            $new_combinations[] = $combination . $char;
+        }
+    }
+
+    # call same function again for the next iteration
+    return sampling($chars, $size - 1, $new_combinations);
+
+}
+
+
+function calculateFiveDigitPasswords($passwordsList) {
+    // calculate all the possible passwords made out of 5 numbers
+    $output = sampling(explode(',', DIGITS), 5);
+
+    $solutions = [];
+
+    foreach ($output as $combination) {
+        $possiblePassword = salter($combination);
+
+        if (checkHashAgainstPasswordHash($possiblePassword, $passwordsList)) {
+            $userId = array_search($possiblePassword, $passwordsList);
+            $solutions[] = [
+                "userID" => $userId,
+                "passwordHash" => $possiblePassword,
+                "actualPassword" => $combination
+            ];
+        }
+    }
+
+    return $solutions;
+}
+
+
+function getResultsTable($resultsArray) {
+    $output = "<table class='table'><tr><th>User ID</th><th>Password Hash</th><th>Actual Password</th></tr>";
+
+    foreach ($resultsArray as $resultRow) {
+        $output .= getTableRow($resultRow);
+    }
+
+    $output .= "</table>";
+    return $output;
+}
+
+
+function getTableRow($result) {
+    return "<tr><td>" . $result["userID"] . "</td><td>" . $result["passwordHash"] . "</td><td>" . $result["actualPassword"] . "</td></tr>";
+}
+
+
+$formSubmitted = false;
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST["form_submit"]) && $_POST["form_submit"] === FORM_SUBMIT_VALUE) {
+        $formSubmitted = true;
+    }
+}
 
 ?>
 
@@ -52,9 +172,26 @@ $conn = null;
     </div>
     <h3>Loaded <span class="badge badge-secondary"><?= count($passwordsList); ?></span> passwords</h3>
 
-    <form action="index.php" method="get" id="password_submit_form">
-        <input type="submit" name="">
+    <form action="index.php" method="POST" id="password_submit_form">
+        <input type="hidden" name="form_submit" value="<?= FORM_SUBMIT_VALUE ?>">
     </form>
+
+    <button class="btn btn-primary" type="submit" form="password_submit_form" value="Submit">Crack Passwords</button>
+
+    <?php
+    if ($formSubmitted)
+    {
+        ?>
+            <h3>Passwords Made of 5 numbers</h3>
+        <?php
+
+        $result = calculateFiveDigitPasswords($passwordsList);
+
+        echo getResultsTable($result);
+
+    }
+    ?>
+
 </div>
 
 
